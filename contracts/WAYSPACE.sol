c500d19e7949f6f10537d6283c98e29e419c2ce2
Sweets Sweetman <sweetmantech@gmail.com>
2022-08-25 13:52:05 -0300

    I add PuzzleDrop contract.

--- contracts/WAYSPACE.sol
+++ contracts/WAYSPACE.sol
@@ -1,20 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import "erc721a/contracts/ERC721A.sol";
-import "./PuzzleTime.sol";
+import "./PuzzleDrop.sol";
 import "./AlbumMetadata.sol";
 
-contract WAYSPACE is ERC721A, PuzzleTime, AlbumMetadata {
-    /// @notice Price for Single
-    uint256 public singlePrice = 22200000000000000;
-    /// @notice Price for Single
-    uint256 public bundlePrice = 33300000000000000;
-
-    /// @notice Wrong price for purchase
-    error Purchase_WrongPrice(uint256 correctPrice);
-
-    constructor(string[] memory _musicMetadata) ERC721A("WAYSPACE", "JACKIE") {
+contract WAYSPACE is PuzzleDrop, AlbumMetadata {
+    constructor(string[] memory _musicMetadata)
+        PuzzleDrop("WAYSPACE", "JACKIE")
+    {
         setupAlbumMetadata(_musicMetadata);
     }
 
@@ -40,11 +33,6 @@ contract WAYSPACE is ERC721A, PuzzleTime, AlbumMetadata {
         //     firstPurchasedTokenId: firstMintedTokenId
         // });
         // return firstMintedTokenId;
-        return 1;
-    }
-
-    /// @notice Returns the starting token ID.
-    function _startTokenId() internal pure override returns (uint256) {
         return 1;
     }
 
