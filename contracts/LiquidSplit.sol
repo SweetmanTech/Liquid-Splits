e2afdc4192651ceff1d9855b742648880e1d2b1c
Sweets Sweetman <sweetmantech@gmail.com>
2022-09-08 18:40:14 -0300

    WIP

--- contracts/LiquidSplit.sol
+++ contracts/LiquidSplit.sol
@@ -26,7 +26,7 @@ contract LiquidSplit is SplitHelpers {
         payoutSplit.transfer(address(this).balance);
         splitMain.updateAndDistributeETH(
             payoutSplit,
-            _uniqueAddresses(sortedAccounts),
+            sortedAccounts,
             percentAllocations,
             0,
             address(0)
